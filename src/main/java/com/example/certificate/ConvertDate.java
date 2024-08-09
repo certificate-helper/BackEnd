ff9f9ec0de6c4cb4d50fc1683210a816d0766ed2
changeme4585 <93308824+changeme4585@users.noreply.github.com>
2024-08-09 13:21:13 +0900

    현재 시간을 정수로 변환해주는 클래스

---
+++ src/main/java/com/example/certificate/ConvertDate.java
@@ -0,0 +1,19 @@
+package com.example.certificate;
+
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class ConvertDate { //현재 시간을 정수로 변환해주는 클래스
+    LocalDateTime date;
+    public  ConvertDate(LocalDateTime date){
+        this.date = date;
+    }
+    public  int  intDate(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String formattedNow = date.format(formatter);
+        return Integer.valueOf(formattedNow);
+    }
+
+}
